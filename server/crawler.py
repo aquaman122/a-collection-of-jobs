c0e856c9940c0edf7599914f71f5a868cd4c21c7
aquaman122 <swj981128@gmail.com>
2025-03-28 11:13:19 +0900

    feat: 잡코리아 크롤링 코드 추가

--- server/crawler.py
+++ server/crawler.py
@@ -1,5 +1,6 @@
 from jobsites.jumpit import scrape_jumpit_jobs
 from jobsites.zighang import scrape_zighang_frontend_jobs
+from jobsites.jobkorea import scrape_jobkorea_frontend_jobs
 
 def collect_all_jobs():
-  return scrape_jumpit_jobs() + scrape_zighang_frontend_jobs()
+  return scrape_jumpit_jobs() + scrape_zighang_frontend_jobs() +  scrape_jobkorea_frontend_jobs()
