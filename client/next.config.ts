c4b8c3c11b60962c79e25985d10e830098a9a573
aquaman122 <swj981128@gmail.com>
2025-05-16 15:41:17 +0900

    feat: turbo 삭제 및 불필요 코드 삭제

--- client/next.config.ts
+++ client/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbo: {
+      loaders: {},
+    },
+  },
 };
 
 export default nextConfig;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -3,7 +3,6 @@ import JobsCard from "@/components/JobsCard";
 export default function Home() {
   return (
     <div>
-      <div>1</div>
       <JobsCard />
     </div>
   );
